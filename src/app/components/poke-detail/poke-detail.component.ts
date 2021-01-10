5daca22d6129accd0d0e245b68dd5d00009e083f
Elias Torres <eliato_@hotmail.com>
2021-01-10 17:56:08 -0600

    create all components

---
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-detail',
+  templateUrl: './poke-detail.component.html'
+})
+export class PokeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/poke-table/poke-table.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-table',
+  templateUrl: './poke-table.component.html'
+})
+export class PokeTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
