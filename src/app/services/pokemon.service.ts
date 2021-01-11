2eeb98eed2e6803f780604cec2bc745672900f77
Elias Torres <eliato_@hotmail.com>
2021-01-10 22:57:44 -0600

    testing

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  //Obtiene pokemon
+  getPokemons(index){
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${index}`);
+  }
+  
 }
